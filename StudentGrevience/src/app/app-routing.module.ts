d743e6b1f759d84766db815e9bc2ce2f2f845ec8
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-12-01 16:42:20 +0530

    route guards

--- StudentGrevience/src/app/app-routing.module.ts
+++ StudentGrevience/src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnectDbComponent } from './connect-db/connect-db.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginGuardService } from './guards/loginguard.service';
+import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { StudentdetailComponent } from './studentdetail/studentdetail.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent,canActivate:[LoginGuardService] },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'students', component: ConnectDbComponent },
+  {path: 'login',component:LoginComponent},
+  { path: 'students', component: ConnectDbComponent ,canActivate:[LoginGuardService] },
   { path: 'detail/:id', component: StudentdetailComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
--- StudentGrevience/src/app/app.module.ts
+++ StudentGrevience/src/app/app.module.ts
@@ -8,8 +8,9 @@ import {HttpClientModule} from '@angular/common/http';
 import { RoutingComponent } from './routing/routing.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StudentdetailComponent } from './studentdetail/studentdetail.component'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +19,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     RoutingComponent,
     DashboardComponent,
     StudentdetailComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
