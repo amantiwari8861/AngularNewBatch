020c8ba1f4dbd34f73e62286cc6a7101778763e2
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-01-24 18:08:35 +0530

    basic io

--- AngularBasics/src/app/app.component.ts
+++ AngularBasics/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { findIndex } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
+  selector: 'Layout',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularBasics';
+export class AppComponent 
+{
+  // num=100;
+  // findIndex():number
+  // {
+  //   console.log("hello there");
+  //   return 100;
+  // }
+
+  // age!:number
+  // age:number=105
+
+  checkAge(e:any)
+  {
+    console.log(e);
+    
+    if (e.target.value>=18) 
+    {
+      console.log("Ready to vote");
+    }
+    else
+    {
+      console.log("not Ready to vote");
+    }
+  }
+  
 }
